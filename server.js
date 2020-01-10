bf1f68518a9e0f9cfeaccecfc177dc45a627b7db
k-lord <katielord27@gmail.com>
2020-01-10 14:17:15 -0500

    added igrid with form and li elements for to index handlebars and added partial burger handlebars

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.json());
 
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ 
+    defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgerController.js");
