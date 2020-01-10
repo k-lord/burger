938f75529975d4dea11ca43dc7787ce97cae75b7
k-lord <katielord27@gmail.com>
2020-01-10 17:01:03 -0500

    resetting server.js

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var http = require('http');
 
 var app = express();
 
-app.use(express.static("public"));
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ 
-    defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+function normalizePort(val) {
+    var port = parseInt(val, 10);
 
-var routes = require("./controllers/burgerController.js");
+    if (isNaN(port)) {
+        return val;
+    }
 
-app.use(routes);
+    if (port >= 0) {
+        return port;
+    }
 
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+
+    app.use(express.static("public"));
+
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    var exphbs = require("express-handlebars");
+
+    app.engine("handlebars", exphbs({
+        defaultLayout: "main"
+    }));
+    app.set("view engine", "handlebars");
+
+    var routes = require("./controllers/burgerController.js");
+
+    app.use(routes);
+
+};
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
 });
+
